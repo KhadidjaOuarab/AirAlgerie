4c9694fe6d7fce2499e406346f6f234eb87070a9
ouarab.khadidja <ouarab.khadidja@gmail.com>
2022-01-07 23:33:17 +0100

    Add Tailwind CSS for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-
+import MenuIcone from './Components/MenuIcone'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
+     <MenuIcone/>
     </div>
   )
 }
---
+++ src/Components/MenuIcone.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function MenuIcone() {
+    return (
+        <div className='bg-red-600'>
+           <p>test</p> 
+        </div>
+    )
+}
+
+export default MenuIcone
+
